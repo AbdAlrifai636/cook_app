542c6440d2d7fa817cba52c27246c8b9a601eec6
abd_alrifai <abd.alrifai963@gmail.com>
2025-01-17 21:50:22 +0300

    second version

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:cooc_project/config/service_locater.dart';
 import 'package:cooc_project/pages/auth_page.dart';
+import 'package:cooc_project/pages/splash_page.dart';
 import 'package:cooc_project/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthPage(),
+      home: SplashPage(),
     );
   }
 }
